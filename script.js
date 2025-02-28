5b19be9d4fbbd8a66e4469ab5ea8382720ff14ee
Malik-Rizwan0 <malikrizwan107@gmail.com>
2025-02-28 13:09:29 +0500

    Responsive

--- script.js
+++ script.js
@@ -36,72 +36,38 @@ navPic.addEventListener("click", () => {
     }
   });
 //  swipper
-const swiper2 = new Swiper('.slider-wrapper2', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 30,
-
-    // Pagination bullets
-    pagination: {
-        el: '.swiper-pagination2',
-        clickable: true,
-        dynamicBullets: true
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next2',
-        prevEl: '.swiper-button-prev2',
-    },
-
-    // Responsive breakpoints
-    breakpoints: {
-        0: {
-            slidesPerView: 1
-        },
-        450: {
-            slidesPerView: 2
-        },
-        768: {
-            slidesPerView: 3
-        },
-        1024: {
-            slidesPerView: 3
-        }
-    }
-});
 
 
-const swiper = new Swiper('.slider-wrapper', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 30,
-    autoplay: {
-        delay: 3000, // Time between slides in milliseconds
-        disableOnInteraction: false, // Keep autoplay running after user interaction
-    },
-    // Pagination bullets
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true
+// const swiper = new Swiper('.slider-wrapper', {
+//     loop: true,
+//     grabCursor: true,
+//     spaceBetween: 30,
+//     autoplay: {
+//         delay: 3000, // Time between slides in milliseconds
+//         disableOnInteraction: false, // Keep autoplay running after user interaction
+//     },
+//     // Pagination bullets
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//         dynamicBullets: true
 
-    },
+//     },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
 
-    // Responsive breakpoints
-    breakpoints: {
-        0: {
-            slidesPerView: 1
-        },
+//     // Responsive breakpoints
+//     breakpoints: {
+//         0: {
+//             slidesPerView: 1
+//         },
 
-        600: {
-            slidesPerView: 2
-        }
-    }
-});
+//         600: {
+//             slidesPerView: 2
+//         }
+//     }
+// });
